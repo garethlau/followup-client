24487cf305e8c62618475d8fed8771f4b5853794
Gareth <Gareth.kh.lau@gmail.com>
2020-05-03 16:32:51 -0600

    Develop basic signup form

---
+++ src/components/Signup.js
@@ -0,0 +1,66 @@
+import React from "react";
+import useFormInput from "../hooks/useFormInput";
+import { InputGroup, Button, Intent } from "@blueprintjs/core";
+import axios from "axios";
+import utils from "../utils";
+import { BASE_URL } from "../constants";
+import { useHistory } from "react-router-dom";
+
+export default function Signup() {
+  const username = useFormInput("");
+  const password = useFormInput("");
+  const firstName = useFormInput("");
+  const lastName = useFormInput("");
+  const email = useFormInput("");
+  const history = useHistory();
+
+  async function signup() {
+    let data = {
+      username: username.value,
+      password: password.value,
+      firstName: firstName.value,
+      lastName: lastName.value,
+      email: email.value,
+    };
+    try {
+      let res = await axios.post(BASE_URL + "/api/v1/auth/signup", data);
+      const { user, token } = res.data;
+      utils.setJWT(token);
+      history.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <InputGroup
+        value={username.value}
+        onChange={username.onChange}
+        placeholder="Username"
+      />
+
+      <InputGroup
+        value={password.value}
+        onChange={password.onChange}
+        placeholder="Password"
+      />
+      <InputGroup
+        value={firstName.value}
+        onChange={firstName.onChange}
+        placeholder="First Name"
+      />
+      <InputGroup
+        value={lastName.value}
+        onChange={lastName.onChange}
+        placeholder="Last Name"
+      />
+      <InputGroup
+        value={email.value}
+        onChange={email.onChange}
+        placeholder="Email"
+      />
+      <Button text="Sign up" onClick={signup} intent={Intent.PRIMARY} />
+    </div>
+  );
+}
