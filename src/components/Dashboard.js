9c36ffc3598e85209214e069435da19627e1bc71
Gareth <Gareth.kh.lau@gmail.com>
2020-05-06 16:24:14 -0600

    Implement filter
    Search bar and filtering logic has been added to the dashboard.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Tag, Button, Intent, HTMLTable, TagInput } from "@blueprintjs/core";
+import { Tag, Button, Intent, HTMLTable, InputGroup } from "@blueprintjs/core";
 import { useHistory, useParams } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "../constants";
 import utils from "../utils";
 import { store } from "../store";
+import useFormInput from "../hooks/useFormInput";
 
 export default function Dashboard() {
   const { orgName } = useParams();
   const [drafts, setDrafts] = useState([]);
   const history = useHistory();
   const { state } = useContext(store);
+  const search = useFormInput("");
 
   useEffect(() => {
     axios
@@ -24,32 +26,52 @@ export default function Dashboard() {
       });
   }, [orgName]);
 
-  async function createDraft() {
-    let config = utils.getJWTConfig();
-    try {
-      let res = await axios.post(
-        BASE_URL + "/api/v1/draft",
-        {
-          orgName,
-        },
-        config
-      );
-      let { draft } = res.data;
-      let draftId = draft._id;
-      history.push(`/${orgName}/compose?draftId=${draftId}`);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   function edit(id) {
     return function () {
       history.push(`/${orgName}/compose?draftId=${id}`);
     };
   }
 
+  function handleQueryChange(e) {
+    search.onChange(e);
+    console.log(e.target.value);
+  }
+
+  function passesFilter(draft) {
+    let latestVersion =
+      draft.versions.length > 0
+        ? draft.versions[draft.versions.length - 1]
+        : null;
+
+    function containsSearch(elem) {
+      return elem.toLowerCase().includes(search.value.toLowerCase());
+    }
+
+    return (
+      (latestVersion && latestVersion.tags.some(containsSearch)) ||
+      (latestVersion &&
+        latestVersion.subject
+          .toLowerCase()
+          .includes(search.value.toLowerCase())) ||
+      (latestVersion && latestVersion.to.some(containsSearch)) ||
+      (latestVersion &&
+        latestVersion.commitMessage
+          .toLowerCase()
+          .includes(search.value.toLowerCase())) ||
+      draft.creator.firstName.toLowerCase().includes(search.value.toLowerCase())
+    );
+  }
+
   return (
     <div style={{ padding: "20px" }}>
+      <div style={{ width: "250px", marginBottom: "20px" }}>
+        <InputGroup
+          onChange={handleQueryChange}
+          value={search.value}
+          placeholder="I'm looking for..."
+        />
+      </div>
+
       <div style={{ overflow: "auto" }}>
         <HTMLTable bordered interactive>
           <thead>
@@ -68,99 +90,100 @@ export default function Dashboard() {
             </tr>
           </thead>
           <tbody>
-            {drafts.map((draft) => (
-              <tr onClick={edit(draft._id)} key={draft._id}>
-                <td>
-                  {draft.creator.firstName + " " + draft.creator.lastName}
-                </td>
-                <td style={{ textAlign: "center" }}>{draft.versions.length}</td>
+            {drafts.map((draft) => {
+              let latestVersion =
+                draft.versions.length > 0
+                  ? draft.versions[draft.versions.length - 1]
+                  : null;
+              if (passesFilter(draft)) {
+                return (
+                  <tr onClick={edit(draft._id)} key={draft._id}>
+                    <td>
+                      {draft.creator.firstName + " " + draft.creator.lastName}
+                    </td>
+                    <td style={{ textAlign: "center" }}>
+                      {draft.versions.length}
+                    </td>
 
-                <td>
-                  {draft.versions.length > 0 &&
-                    new Date(
-                      draft.versions[draft.versions.length - 1].sendDate
-                    ).toLocaleDateString()}
-                </td>
+                    <td>
+                      {latestVersion &&
+                        new Date(latestVersion.sendDate).toLocaleDateString()}
+                    </td>
 
-                <td>
-                  {draft.versions.length > 0 &&
-                    draft.versions[draft.versions.length - 1].to.map(
-                      (recipient, index) => (
-                        <Tag minimal key={index} style={{ marginRight: "5px" }}>
-                          {recipient}
-                        </Tag>
-                      )
-                    )}
-                </td>
+                    <td>
+                      {latestVersion &&
+                        latestVersion.to.map((recipient, index) => (
+                          <Tag
+                            minimal
+                            key={index}
+                            style={{ marginRight: "5px" }}
+                          >
+                            {recipient}
+                          </Tag>
+                        ))}
+                    </td>
 
-                <td>
-                  {draft.versions.length > 0
-                    ? draft.versions[draft.versions.length - 1].subject
-                    : ""}
-                </td>
+                    <td>{latestVersion ? latestVersion.subject : ""}</td>
 
-                <td>
-                  {draft.versions.length > 0
-                    ? new Date(
-                        parseInt(
-                          draft.versions[draft.versions.length - 1].modifiedDate
-                        )
-                      ).toLocaleDateString()
-                    : "N/A"}
-                </td>
+                    <td>
+                      {latestVersion
+                        ? new Date(
+                            parseInt(latestVersion.modifiedDate)
+                          ).toLocaleDateString()
+                        : "N/A"}
+                    </td>
 
-                <td>
-                  {draft.versions.length > 0 &&
-                    draft.versions[draft.versions.length - 1].commitMessage}
-                </td>
+                    <td>{latestVersion && latestVersion.commitMessage}</td>
 
-                <td>
-                  {draft.versions.length > 0 &&
-                    draft.versions[draft.versions.length - 1].tags.map(
-                      (tag, index) => (
-                        <Tag minimal style={{ marginRight: "5px" }} key={index}>
-                          {tag}
-                        </Tag>
-                      )
-                    )}
-                </td>
+                    <td>
+                      {latestVersion &&
+                        latestVersion.tags.map((tag, index) => (
+                          <Tag
+                            minimal
+                            style={{ marginRight: "5px" }}
+                            key={index}
+                          >
+                            {tag}
+                          </Tag>
+                        ))}
+                    </td>
 
-                <td>
-                  {draft.reviewers.map((reviewer) => {
-                    return (
-                      <Tag
-                        key={reviewer.id}
-                        style={{ marginRight: "5px" }}
-                        intent={
-                          reviewer.approved ? Intent.SUCCESS : Intent.DANGER
-                        }
-                        minimal={!state.user || reviewer.id !== state.user._id}
-                      >
-                        {reviewer.firstName[0] + reviewer.lastName[0]}
-                      </Tag>
-                    );
-                  })}
-                </td>
+                    <td>
+                      {draft.reviewers.map((reviewer) => {
+                        return (
+                          <Tag
+                            key={reviewer.id}
+                            style={{ marginRight: "5px" }}
+                            intent={
+                              reviewer.approved ? Intent.SUCCESS : Intent.DANGER
+                            }
+                            minimal={
+                              !state.user || reviewer.id !== state.user._id
+                            }
+                          >
+                            {reviewer.firstName[0] + reviewer.lastName[0]}
+                          </Tag>
+                        );
+                      })}
+                    </td>
 
-                <td style={{ textAlign: "center" }}>
-                  {draft.reviewers.reduce((acc, curr) => {
-                    if (curr.approved) acc++;
-                    return acc;
-                  }, 0)}
-                  / {draft.reviewers.length}
-                </td>
+                    <td style={{ textAlign: "center" }}>
+                      {draft.reviewers.reduce((acc, curr) => {
+                        if (curr.approved) acc++;
+                        return acc;
+                      }, 0)}
+                      / {draft.reviewers.length}
+                    </td>
 
-                <td>{draft.sent ? "Sent" : "Not Sent"}</td>
-              </tr>
-            ))}
+                    <td>{draft.sent ? "Sent" : "Not Sent"}</td>
+                  </tr>
+                );
+              }
+              else return null;
+            })}
           </tbody>
         </HTMLTable>
       </div>
-      <Button
-        text="Create Draft"
-        intent={Intent.PRIMARY}
-        onClick={createDraft}
-      />
     </div>
   );
 }
