0215e27a62906e10f1e08c772ca66ffa28e4bcbf
Gareth <Gareth.kh.lau@gmail.com>
2020-05-05 23:01:14 -0600

    Implement table view

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { Button, Intent, HTMLTable } from "@blueprintjs/core";
+import React, { useState, useEffect, useContext } from "react";
+import { Tag, Button, Intent, HTMLTable, TagInput } from "@blueprintjs/core";
 import { useHistory, useParams } from "react-router-dom";
 import axios from "axios";
 import { BASE_URL } from "../constants";
 import utils from "../utils";
+import { store } from "../store";
 
 export default function Dashboard() {
   const { orgName } = useParams();
   const [drafts, setDrafts] = useState([]);
   const history = useHistory();
+  const { state } = useContext(store);
 
   useEffect(() => {
     axios
@@ -40,28 +42,120 @@ export default function Dashboard() {
     }
   }
 
+  function edit(id) {
+    return function () {
+      history.push(`/${orgName}/compose?draftId=${id}`);
+    };
+  }
+
   return (
-    <div>
-      <HTMLTable>
-        <thead>
-          <tr>
-            <th>Creator</th>
-            <th>Versions</th>
-            <th>Sent?</th>
-            <th>Reviewers</th>
-          </tr>
-        </thead>
-        <tbody>
-          {drafts.map((draft) => (
+    <div style={{ padding: "20px" }}>
+      <div style={{ overflow: "auto" }}>
+        <HTMLTable bordered interactive>
+          <thead>
             <tr>
-              <td>{draft.creator.firstName + " " + draft.creator.lastName}</td>
-              <td>{draft.versions.length}</td>
-              <td>{draft.sent}</td>
-              <td>{draft.reviewers}</td>
+              <th style={{ minWidth: "150px" }}>Creator</th>
+              <th>Versions</th>
+              <th>Send Date</th>
+              <th>To</th>
+              <th style={{ width: "250px" }}>Subject</th>
+              <th>Last Modified</th>
+              <th style={{ minWidth: "250px" }}>Latest Commit Message</th>
+              <th>Tags</th>
+              <th style={{ minWidth: "150px" }}>Reviewers</th>
+              <th>Approvals</th>
+              <th>Status</th>
             </tr>
-          ))}
-        </tbody>
-      </HTMLTable>
+          </thead>
+          <tbody>
+            {drafts.map((draft) => (
+              <tr onClick={edit(draft._id)} key={draft._id}>
+                <td>
+                  {draft.creator.firstName + " " + draft.creator.lastName}
+                </td>
+                <td style={{ textAlign: "center" }}>{draft.versions.length}</td>
+
+                <td>
+                  {draft.versions.length > 0 &&
+                    new Date(
+                      draft.versions[draft.versions.length - 1].sendDate
+                    ).toLocaleDateString()}
+                </td>
+
+                <td>
+                  {draft.versions.length > 0 &&
+                    draft.versions[draft.versions.length - 1].to.map(
+                      (recipient, index) => (
+                        <Tag minimal key={index} style={{ marginRight: "5px" }}>
+                          {recipient}
+                        </Tag>
+                      )
+                    )}
+                </td>
+
+                <td style={{ textAlign: "center" }}>
+                  {draft.versions.length > 0
+                    ? draft.versions[draft.versions.length - 1].subject
+                    : ""}
+                </td>
+
+                <td>
+                  {draft.versions.length > 0
+                    ? new Date(
+                        Date(
+                          draft.versions[draft.versions.length - 1].modifiedDate
+                        )
+                      ).toLocaleDateString()
+                    : "N/A"}
+                </td>
+
+                <td>
+                  {draft.versions.length > 0 &&
+                    draft.versions[draft.versions.length - 1].commitMessage}
+                </td>
+
+                <td>
+                  {draft.versions.length > 0 &&
+                    draft.versions[draft.versions.length - 1].tags.map(
+                      (tag, index) => (
+                        <Tag minimal style={{ marginRight: "5px" }} key={index}>
+                          {tag}
+                        </Tag>
+                      )
+                    )}
+                </td>
+
+                <td>
+                  {draft.reviewers.map((reviewer) => {
+                    return (
+                      <Tag
+                        key={reviewer.id}
+                        style={{ marginRight: "5px" }}
+                        intent={
+                          reviewer.approved ? Intent.SUCCESS : Intent.DANGER
+                        }
+                        minimal={reviewer.id !== state.user._id}
+                      >
+                        {reviewer.firstName[0] + reviewer.lastName[0]}
+                      </Tag>
+                    );
+                  })}
+                </td>
+
+                <td style={{ textAlign: "center" }}>
+                  {draft.reviewers.reduce(
+                    (acc, curr) => (curr.approved ? acc++ : acc),
+                    0
+                  )}{" "}
+                  / {draft.reviewers.length}
+                </td>
+
+                <td>{draft.sent ? "Sent" : "Not Sent"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </HTMLTable>
+      </div>
       <Button
         text="Create Draft"
         intent={Intent.PRIMARY}
