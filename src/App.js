1f54555cc11175fa2ef234ab3d892f0667a0f739
Gareth <Gareth.kh.lau@gmail.com>
2020-05-09 13:19:38 -0600

    Attempt to refresh access token on app load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
@@ -14,7 +14,57 @@ import Nav from "./components/Nav";
 import OrgCreate from "./components/OrgCreate";
 import { StateProvider } from "./store.js";
 
+import axios from "axios";
+import { BASE_URL } from "./constants";
+import { setAccessToken } from "./accessToken";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .post(`${BASE_URL}/api/v1/auth/refresh-token`, null, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data && res.data.accessToken) {
+          setAccessToken(res.data.accessToken);
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    const REFRESH_TIME = 10 * 60 * 1000; // Ten Minutes
+    const interval = setInterval(() => {
+      axios
+        .post(`${BASE_URL}/api/v1/auth/refresh-token`, null, {
+          withCredentials: true,
+        })
+        .then((res) => {
+          console.log(res.data);
+          if (res.data && res.data.accessToken) {
+            setAccessToken(res.data.accessToken);
+          }
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }, REFRESH_TIME);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <StateProvider>
       <Router>
