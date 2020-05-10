90f42cee81363d25e43753652a0eb5f072428fae
Gareth <Gareth.kh.lau@gmail.com>
2020-05-09 20:51:04 -0600

    Error handling, UI improvements, and fix missing redirect query

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react";
-import { InputGroup, Button } from "@blueprintjs/core";
+import React, { useContext, useState } from "react";
+import { InputGroup, Button, Intent, Tooltip } from "@blueprintjs/core";
 import useFormInput from "../hooks/useFormInput";
 import { BASE_URL } from "../constants";
 import axios from "axios";
@@ -7,6 +7,7 @@ import { useHistory } from "react-router-dom";
 import { store, actions } from "../store";
 import { setAccessToken } from "../accessToken";
 import utils from "../utils";
+import { AppToaster } from "../toaster";
 
 export default function Login() {
   const { dispatch } = useContext(store);
@@ -14,10 +15,16 @@ export default function Login() {
   const username = useFormInput("");
   const password = useFormInput("");
   const history = useHistory();
+  const [showPassword, setShowPassword] = useState(false);
 
   async function login() {
-    const search = utils.searchToJSON(history.location.search);
-    const redirect = search.redirect || "/create";
+    let redirect = null;
+    try {
+      const search = utils.searchToJSON(history.location.search);
+      redirect = search.redirect || "/create";
+    } catch {
+      redirect = "/create";
+    }
 
     let data = {
       username: username.value,
@@ -26,28 +33,98 @@ export default function Login() {
     try {
       let result = await axios.post(BASE_URL + "/api/v1/auth/login", data);
       const { accessToken, user } = result.data;
-      setAccessToken(accessToken);
-      dispatch({ type: actions.SET_USER, payload: user });
-      // TODO Update redirect
-      history.push(redirect);
+      if (accessToken) {
+        setAccessToken(accessToken);
+        dispatch({ type: actions.SET_USER, payload: user });
+        history.push(redirect);
+      } else {
+        console.log("Invalid login");
+      }
     } catch (err) {
       console.log(err.message);
+      if (err.response) {
+        console.log(err.response.status);
+        if (err.response.status === 404) {
+          AppToaster.show({
+            message: "User does not exist.",
+            intent: Intent.DANGER,
+          });
+        } else if (err.response.status === 401) {
+          AppToaster.show({
+            message: "Incorrect password.",
+            intent: Intent.DANGER,
+          });
+        } else {
+          AppToaster.show({
+            message: "There was an error. Please try again.",
+            intent: Intent.DANGER,
+          });
+        }
+      }
     }
   }
 
+  function goSignup() {
+    if (history.location.search) {
+      const search = utils.searchToJSON(history.location.search);
+      let redirect = search.redirect;
+      history.push(`/signup?redirect=${redirect}`);
+    } else {
+      history.push("/signup");
+    }
+  }
+  function handleLockClick() {
+    setShowPassword(!showPassword);
+  }
+
+  const lockButton = (
+    <Tooltip content={`${showPassword ? "Hide" : "Show"} Password`}>
+      <Button
+        icon={showPassword ? "unlock" : "lock"}
+        intent={Intent.WARNING}
+        minimal={true}
+        onClick={handleLockClick}
+      />
+    </Tooltip>
+  );
+
   return (
     <div>
-      <InputGroup
-        placeholder="Username"
-        value={username.value}
-        onChange={username.onChange}
-      />
-      <InputGroup
-        placeholder="Password"
-        value={password.value}
-        onChange={password.onChange}
-      />
-      <Button text="Log in" onClick={login} />
+      <div
+        style={{
+          width: "550px",
+          height: "auto",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          padding: "30px",
+        }}
+      >
+        <h1>Log in</h1>
+        <InputGroup
+          style={{ marginBottom: "20px" }}
+          placeholder="Username"
+          value={username.value}
+          onChange={username.onChange}
+        />
+        <InputGroup
+          style={{ marginBottom: "20px" }}
+          placeholder="Password"
+          value={password.value}
+          onChange={password.onChange}
+          rightElement={lockButton}
+          type={showPassword ? "text" : "password"}
+        />
+        <div style={{ textAlign: "right" }}>
+          <Button
+            style={{ marginRight: "10px" }}
+            text="Sign up"
+            onClick={goSignup}
+          />
+          <Button text="Log in" onClick={login} intent={Intent.PRIMARY} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import useFormInput from "../hooks/useFormInput";
-import { InputGroup, Button, Intent } from "@blueprintjs/core";
+import { InputGroup, Button, Intent, Tooltip } from "@blueprintjs/core";
 import axios from "axios";
 import { BASE_URL } from "../constants";
 import { useHistory } from "react-router-dom";
 import { store, actions } from "../store";
 import { setAccessToken } from "../accessToken";
+import utils from "../utils";
+import { AppToaster } from "../toaster";
 
 export default function Signup() {
   const { dispatch } = useContext(store);
@@ -16,8 +18,17 @@ export default function Signup() {
   const lastName = useFormInput("");
   const email = useFormInput("");
   const history = useHistory();
+  const [showPassword, setShowPassword] = useState(false);
 
   async function signup() {
+    let redirect = null;
+    try {
+      const search = utils.searchToJSON(history.location.search);
+      redirect = search.redirect || "/create";
+    } catch {
+      redirect = "/create";
+    }
+
     let data = {
       username: username.value,
       password: password.value,
@@ -31,41 +42,100 @@ export default function Signup() {
       setAccessToken(accessToken);
       dispatch({ type: actions.SET_USER, payload: user });
 
-      history.push("/dashboard");
+      history.push(redirect);
     } catch (err) {
       console.log(err.message);
+      if (err.response) {
+        if (err.response.status === 422) {
+          AppToaster.show({
+            message: err.response.data.message,
+            intent: Intent.DANGER,
+          });
+        }
+      }
     }
   }
 
+  function goLogin() {
+    if (history.location.search) {
+      const search = utils.searchToJSON(history.location.search);
+      let redirect = search.redirect;
+      history.push(`/login?redirect=${redirect}`);
+    } else {
+      history.push("/login");
+    }
+  }
+
+  function handleLockClick() {
+    setShowPassword(!showPassword);
+  }
+
+  const lockButton = (
+    <Tooltip content={`${showPassword ? "Hide" : "Show"} Password`}>
+      <Button
+        icon={showPassword ? "unlock" : "lock"}
+        intent={Intent.WARNING}
+        minimal={true}
+        onClick={handleLockClick}
+      />
+    </Tooltip>
+  );
+  const inputStyle = { marginBottom: "20px" };
   return (
     <div>
-      <InputGroup
-        value={username.value}
-        onChange={username.onChange}
-        placeholder="Username"
-      />
-
-      <InputGroup
-        value={password.value}
-        onChange={password.onChange}
-        placeholder="Password"
-      />
-      <InputGroup
-        value={firstName.value}
-        onChange={firstName.onChange}
-        placeholder="First Name"
-      />
-      <InputGroup
-        value={lastName.value}
-        onChange={lastName.onChange}
-        placeholder="Last Name"
-      />
-      <InputGroup
-        value={email.value}
-        onChange={email.onChange}
-        placeholder="Email"
-      />
-      <Button text="Sign up" onClick={signup} intent={Intent.PRIMARY} />
+      <div
+        style={{
+          width: "550px",
+          height: "auto",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          padding: "30px",
+        }}
+      >
+        <h1>Sign up</h1>
+        <InputGroup
+          style={inputStyle}
+          value={firstName.value}
+          onChange={firstName.onChange}
+          placeholder="First Name"
+        />
+        <InputGroup
+          style={inputStyle}
+          value={lastName.value}
+          onChange={lastName.onChange}
+          placeholder="Last Name"
+        />
+        <InputGroup
+          style={inputStyle}
+          value={email.value}
+          onChange={email.onChange}
+          placeholder="Email"
+        />
+        <InputGroup
+          style={inputStyle}
+          value={username.value}
+          onChange={username.onChange}
+          placeholder="Username"
+        />
+        <InputGroup
+          style={inputStyle}
+          value={password.value}
+          onChange={password.onChange}
+          placeholder="Password"
+          rightElement={lockButton}
+          type={showPassword ? "text" : "password"}
+        />
+        <div style={{ textAlign: "right" }}>
+          <Button
+            text="Log in"
+            onClick={goLogin}
+            style={{ marginRight: "10px" }}
+          />
+          <Button text="Sign up" onClick={signup} intent={Intent.PRIMARY} />
+        </div>
+      </div>
     </div>
   );
 }
