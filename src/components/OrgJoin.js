06c4258745e8f42cb3d6b51faab272bd8a8d6c30
Gareth <Gareth.kh.lau@gmail.com>
2020-05-09 21:11:08 -0600

    Add error handling and improved styling

--- src/components/OrgJoin.js
+++ src/components/OrgJoin.js
@@ -1,32 +1,79 @@
 import React from "react";
 import { InputGroup, Button, Intent } from "@blueprintjs/core";
 import useFormInput from "../hooks/useFormInput";
-import utils from "../utils";
 import axios from "axios";
 import { BASE_URL } from "../constants";
+import { AppToaster } from "../toaster";
+import { useHistory } from "react-router-dom";
 
 export default function OrgJoin() {
+  const history = useHistory();
   const orgName = useFormInput("");
 
   async function join() {
+    if (orgName.value.trim() === "") {
+      AppToaster.show({
+        message: "Organization name cannot be blank.",
+        intent: Intent.DANGER,
+      });
+      return;
+    }
     try {
-      let result = await axios.patch(
-        `${BASE_URL}/api/v1/organization/${orgName.value}/users`
-      );
-      console.log(result);
+      let name = orgName.value.replace(/ /g, "-");
+      await axios.patch(`${BASE_URL}/api/v1/organization/${name}/users`);
+      AppToaster.show({
+        message: `You've sucessfully joined ${name}. Redirecting to dashboard.`,
+        intent: Intent.SUCCESS,
+      });
+      setTimeout(() => {
+        history.push(`/${name}/dashboard`);
+      }, 3000);
     } catch (err) {
-      console.log(err.message);
+      if (err.response) {
+        if (err.response.status === 404) {
+          AppToaster.show({
+            message: "Organization does not exist.",
+            intent: Intent.DANGER,
+          });
+        } else if (err.response.status === 401) {
+          AppToaster.show({
+            message: "You need to be logged in to join an organization.",
+            intent: Intent.DANGER,
+            action: {
+              onClick: () => history.push("/login?redirect=/join"),
+              text: "Log in",
+            },
+          });
+        }
+      }
     }
   }
 
   return (
     <div>
-      <InputGroup
-        placeholder="Organization Name"
-        value={orgName.value}
-        onChange={orgName.onChange}
-      />
-      <Button intent={Intent.PRIMARY} onClick={join} text="Join" />
+      <div
+        style={{
+          width: "550px",
+          height: "auto",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          padding: "30px",
+        }}
+      >
+        <h1>Join an organization</h1>
+        <p>Enter the organization name you want to join. </p>
+        <InputGroup
+          style={{ marginBottom: "20px" }}
+          placeholder="my-cool-organization"
+          value={orgName.value.replace(/ /g, "-")}
+          onChange={orgName.onChange}
+        />
+        <div style={{ textAlign: "right" }}>
+          <Button intent={Intent.PRIMARY} onClick={join} text="Join" />
+        </div>
+      </div>
     </div>
   );
 }
